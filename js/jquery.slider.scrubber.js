b9f9f3bc392c2b01fa6f38469d8312c14f8ffbf3
Jun Zheng <arthurjordan@gmail.com>
2015-08-25 16:28:21 +1000

    thoroughly cleaned up and refactored the jquery.slider.scrubber.js plugin

--- js/jquery.slider.scrubber.js
+++ js/jquery.slider.scrubber.js
@@ -1,5 +1,5 @@
 /**
- * Created by dev on 20/08/15.
+ * Created by Jun on 20/08/15.
  */
 (function ($) {
     $.fn.scrubber = function (options) {
@@ -10,50 +10,51 @@
 
         // the scrub function that produces the image scrubbing effect;
         function _scrub(itemToShow) {
+            //using the $GoTo method from the API; slider is the slider object passed in;
             settings.slider.$GoTo(itemToShow);
+            // remove the thumbnail-hover class from all elements, just show only the current thumbnail has the white border;
             $(".c").removeClass('thumbnail-hover');
+            //add the thumbnail-hover class to the image to be shown;
             $($(".c")[itemToShow]).addClass("thumbnail-hover");
         }
 
         // function to calculate the index of image to show;
-        function _calculateIndex(e, $this, horizontalTrigger, numberOfChildren) {
-            var remaing_index, index;
-            remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
-            index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
-            index -= 1; // index for 0 based array to use;
+        function _calculateIndex(e, $this) {
+            // select all the divs;
+            var elements = $("div", $this);
+
+            // calculate the width of the div;
+            var element_width = elements.first().width();
+
+            // using the $SlidesCount() method from the API to calculate the number of divs;
+            var numberOfChildren = settings.slider.$SlidesCount();
+
+            // get the trigger size => scrubber size (div size) / number of chidren;
+            var horizontalTrigger = element_width / numberOfChildren;
+
+            // calculate the remaining number of divs;
+            var remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
+            // calculate the index of the div to be shown;
+            var index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
+            index -= 1; // index of div to be shown is 0 based;
             return index;
         }
 
         return this.each(function () {
-            var $this, element_width, elements, horizontalTrigger;
+            var $this = $(this);
 
-
-            $this = $(this);
-
-            elements = $("div", $this);
-
-            element_width = elements.first().width();
-
-            // using the $SlidesCount() method from the API;
-            numberOfChildren = settings.slider.$SlidesCount();
-
-            // get the trigger size => scrubber size / number of chidren;
-            horizontalTrigger = element_width / numberOfChildren;
-
-            // bind mousemove event with the scrubber function;
+            // bind mousemove and moouseleave event of the slider div with the scrubber function;
             $this.on('mousemove.scrubber', function (e) {
-                return _scrub(_calculateIndex(e, $this, horizontalTrigger, numberOfChildren));
-            });
-
-            $("div[u=thumbnavigator]").on('mousemove.scrubber', function (e) {
-                return _scrub(_calculateIndex(e, $this, horizontalTrigger, numberOfChildren));
+                return _scrub(_calculateIndex(e, $this));
             }).on("mouseleave.scrubber", function () {
-                return _scrub(0);
+                return _scrub(0); //0 means showing the first image;
             });
 
-            // bind mouseleve event with the scrubber function;
-            return $this.on("mouseleave.scrubber", function () {
-                return _scrub(0);
+            // bind mousemove and moouseleave event of the thumbnail div with the scrubber function;
+            $("div[u=thumbnavigator]").on('mousemove.scrubber', function (e) {
+                return _scrub(_calculateIndex(e, $this));
+            }).on("mouseleave.scrubber", function () {
+                return _scrub(0); //0 means showing the first image;
             });
         });
     };
