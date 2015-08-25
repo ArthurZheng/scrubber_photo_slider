bf0326e0d50788f233a49eb9cbb17d8cc0bcd10e
Jun Zheng <arthurjordan@gmail.com>
2015-08-25 12:36:02 +1000

    extracted the function _calculateIndex()

--- js/jquery.slider.scrubber.js
+++ js/jquery.slider.scrubber.js
@@ -9,47 +9,53 @@
         }, options);
 
         // the scrub function that produces the image scrubbing effect;
-        var scrub = function(elements, itemToShow){
+        //var scrub = function(elements, itemToShow){
+        //    settings.slider.$GoTo(itemToShow - 1);
+        //};
+        //
+        function _scrub(elements, itemToShow){
             settings.slider.$GoTo(itemToShow - 1);
         };
 
-        return this.each(function(){
-            var $this, elements, horizontalTrigger;
+        // function to calculate the index of image to show;
+        function _calculateIndex(e, $this, horizontalTrigger, numberOfChildren) {
+            var remaing_index, index;
+            remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
+            index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
+            return index;
+        }
 
-                // element_width, element_height, numberOfChildren;
+        return this.each(function(){
+            var $this, element_width, elements, horizontalTrigger;
+
 
             $this = $(this);
 
-            console.log("$this is ", $this);
-
             elements = $("div", $this);
 
-            //elements = $this.find("li");
             element_width = elements.first().width();
-            element_height = elements.first().height();
-            //$this.width(element_width).height(element_height).css('padding', 0);
-            //numberOfChildren = elements.length;
+
+            // using the $SlidesCount() method from the API;
             numberOfChildren = settings.slider.$SlidesCount();
-            //numberOfChildren = $this.children().length;
 
             // get the trigger size => scrubber size / number of chidren;
             horizontalTrigger = element_width / numberOfChildren;
 
-            // show the first element
-            //scrub(elements, 1);
-
             // bind mousemove event with the scrubber function;
             $this.on('mousemove.scrubber', function(e){
-                var remaing_index, index;
-                remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
-                index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
-                return scrub(elements, index);
+                //var remaing_index, index;
+                //remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
+                //index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
+
+                var index;
+                index = _calculateIndex(e, $this, horizontalTrigger, numberOfChildren);
+                return _scrub(elements, index);
             });
 
 
             // bind mouse
             return $this.on("mouseleave.scrubber", function(){
-                return scrub(elements, 1);
+                return _scrub(elements, 1);
             });
         });
     };
