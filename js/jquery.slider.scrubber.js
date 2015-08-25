d52e3496c61ef9bc3709ada9b873b0dd1e9ad735
Jun Zheng <arthurjordan@gmail.com>
2015-08-25 16:15:40 +1000

    cleaned up jquery.slider.scrubber.js

--- js/jquery.slider.scrubber.js
+++ js/jquery.slider.scrubber.js
@@ -1,31 +1,30 @@
 /**
  * Created by dev on 20/08/15.
  */
-(function($){
-    $.fn.scrubber = function( options ) {
+(function ($) {
+    $.fn.scrubber = function (options) {
         var settings = $.extend({
-            showItem : 1,
-            leaveToFirst : true
+            showItem: 1,
+            leaveToFirst: true
         }, options);
 
         // the scrub function that produces the image scrubbing effect;
-        function _scrub(elements, itemToShow){
+        function _scrub(itemToShow) {
             settings.slider.$GoTo(itemToShow);
-            //$().addClass().remveClass()
-            $('.c').css("border", "#fff 0px solid").css("width", "68px").css("height", "68px").css("top", "2px").css("left", "2px");
-            $($('.c')[itemToShow]).css("border", "#fff 1px solid").css("width", "70px").css("height", "70px").css("top", "0").css("left", "0");
-        };
+            $(".c").removeClass('thumbnail-hover');
+            $($(".c")[itemToShow]).addClass("thumbnail-hover");
+        }
 
         // function to calculate the index of image to show;
         function _calculateIndex(e, $this, horizontalTrigger, numberOfChildren) {
             var remaing_index, index;
             remaing_index = Math.ceil((e.pageX - $this.offset().left) / horizontalTrigger);
             index = Math.min(Math.max(remaing_index, 1), numberOfChildren);
-            index -= 1;
+            index -= 1; // index for 0 based array to use;
             return index;
         }
 
-        return this.each(function(){
+        return this.each(function () {
             var $this, element_width, elements, horizontalTrigger;
 
 
@@ -42,15 +41,20 @@
             horizontalTrigger = element_width / numberOfChildren;
 
             // bind mousemove event with the scrubber function;
-            $this.on('mousemove.scrubber', function(e){
-                var index = _calculateIndex(e, $this, horizontalTrigger, numberOfChildren);
-                return _scrub(elements, index);
+            $this.on('mousemove.scrubber', function (e) {
+                return _scrub(_calculateIndex(e, $this, horizontalTrigger, numberOfChildren));
+            });
+
+            $("div[u=thumbnavigator]").on('mousemove.scrubber', function (e) {
+                return _scrub(_calculateIndex(e, $this, horizontalTrigger, numberOfChildren));
+            }).on("mouseleave.scrubber", function () {
+                return _scrub(0);
             });
 
             // bind mouseleve event with the scrubber function;
-            return $this.on("mouseleave.scrubber", function(){
-                return _scrub(elements, 0);
+            return $this.on("mouseleave.scrubber", function () {
+                return _scrub(0);
             });
         });
     };
-}( jQuery ));
+}(jQuery));
